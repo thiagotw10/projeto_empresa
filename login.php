0df6bc23be67aaecaa5d6bf3453d846228b96e44
thiagotw10 <thiagotw10@hotmail.com>
2021-02-26 15:44:08 -0300

    projeto_empresa

--- login.php
+++ login.php
@@ -5,3 +5,14 @@ if(empty($_POST['usuario']) || empty ($_POST['senha'])){
     header('location: index.html');
     exit();
 }
+
+$usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
+$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+
+$query = " select usuario_id, usuario from usuario where usuario = '{$usuario}' and senha = md5('{$senha}')";
+
+$result = mysqli_query($conexao, $query);
+
+$row = mysqli_num_rows($result);
+
+echo $row;exit;
